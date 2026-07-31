269b167597f8ebf26aaa5c3580359b4b9293e661
amandangi202300@gmsil.com <amandangi202300@gmail.com>
2026-07-31 22:54:09 +0530

    math function in jaba

---
+++ Math1.java
@@ -0,0 +1,16 @@
+  
+public class Math1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Square Root Of number 25 is :- " + Math.sqrt(25));
+        System.out.println("Power of a number 5 is :- " + Math.pow(5,2));
+        System.out.println("Maximum No of :- "+ Math.max(10,20));
+        System.out.println("Minimum No of :- "+ Math.min(10,20));
+        System.out.println("Celling value of 12.3 is ;- " + Math.ceil(12.3));
+        System.out.println("Floor Vlaue :- "+ Math.floor(12.8));
+        System.out.println(Math.PI);
+
+
+    }
+}
