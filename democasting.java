1a34a8f22d70eaad421284bc861c3a4e00720a35
Aman <amandangi202300@gmail.com>
2026-08-24 14:50:07 +0530

    explicit conversion

--- democasting.java
+++ democasting.java
@@ -8,9 +8,21 @@ public class democasting {
     //  int a=b;
     //  System.out.println(b);
      // chartaer to init
-      char c = 's';
-      int i = c;
-      System.out.println(i);
-
+    //   char c = 's';
+    //   int i = c;
+    //   System.out.println(i);
+    // Explicit Conversion
+    // int i =24;
+    // byte b=(byte)i;
+    // System.out.println(b);
+    // in this conversion int in range off byte so esaliy convert int to byte
+    // now
+    int i =300;
+    byte b= (byte)i;
+    System.out.println(b); // output is 44 
+    // in this case is out of byte range -128 to 127 so now he covert the int
+    //  to byte like this i ka value mode of 256 eg 300 % 256=44.
+    // second way to conversion 
+    
    }
 }
