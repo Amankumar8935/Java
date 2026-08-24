cb85b5e4c3439b1cf8dd65677dd060c2a6383fc8
Aman <amandangi202300@gmail.com>
2026-08-24 14:07:29 +0530

    Typecasting file

---
+++ democasting.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class democasting {
+   public static void main(String[] args) {
+      //Implicit Conversion 
+      //(Byte into Int)
+      byte b= 89;
+     int a=b;
+     System.out.println(b);
+   }
+}
