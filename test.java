f68a7ae008a988cb65812a10aa13d969485100c8
Aman <amandangi202300@gmail.com>
2026-09-13 18:03:54 +0530

    class object

---
+++ test.java
@@ -0,0 +1,19 @@
+class Demo 
+{
+   int a=10; String b="ankush";
+
+   void show()
+   {
+       System.out.println(a+" "+b);
+   }
+}
+
+
+class test 
+{
+    public static void main(String args[])
+    {
+       Demo r = new Demo();
+       r.show();
+    }
+}
