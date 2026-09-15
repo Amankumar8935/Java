5bbed89b21ef8ce8372ffed6e137937c811db80b
Aman <amandangi202300@gmail.com>
2026-09-15 23:48:18 +0530

    hello

---
+++ mhello.java
@@ -0,0 +1,13 @@
+public class mhello {
+
+    static void number(int a,int b){
+
+        int sum= a+b;
+        System.out.println("sum:- " + sum);
+    }
+        public static void  main(String args[])
+        {
+            number(5,6);
+            number(99,45);
+        }
+}
